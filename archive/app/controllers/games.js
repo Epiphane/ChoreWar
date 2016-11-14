cdd506ee1fcd7f386b59ff194dbc140fc90d5f0a
Thomas Steinke <exyphnos@gmail.com>
2016-11-14 15:17:26 -0800

    gbye archive

--- archive/app/controllers/games.js
+++
@@ -1,3 +0,0 @@
-angular.module('thomassteinke').controller('GamesCtrl', function($scope, games) {
-  $scope.gameInfo = ['threetris', 'rgbzero', 'kickbox', 'spellbound', 'quicksilver', 'sabotage', 'flux', 'knights'];
-});
